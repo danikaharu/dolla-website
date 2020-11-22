cca3360512a6bed049871a48ba33826067f6454e
danikaharu11 <clashhaki55@gmail.com>
2020-11-22 22:20:44 +0800

    Add data to info component

---
+++ src/components/Info/Data.js
@@ -0,0 +1,20 @@
+import svg1 from "../../assets/images/svg-1.svg";
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topline: "Premium Bank",
+  headline: "Unlimited transaction with zero fees",
+  description: `Get access to our exclusive app that 
+    allows you to send unlimited transactions without getting
+    charged my fees`,
+  buttonLabel: "Get  started",
+  imgStart: true,
+  img: svg1,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
